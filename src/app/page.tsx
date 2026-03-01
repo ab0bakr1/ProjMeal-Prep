1396d705c5b29139e3e4b5fc62a01aea201da173
eslam-mohamedl <ee.eslam355@gmail.com>
2026-03-01 09:40:36 +0200

    chore(struture): add all folder for struture and codes in components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import Text from "@/components/atoms/Text";
+import Title from "@/components/atoms/Title";
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <Text variant="primary" size="md">
+        this is text
+      </Text>
+      <Text variant="secondary" size="lg">
+        this is text
+      </Text>
+      <Title variant="black" size="sm">
+        this is heading
+      </Title>
+      <Title variant="gray" size="md">
+        this is heading
+      </Title>
+      <Title variant="primary" size="lg">
+        this is heading
+      </Title>
+      <Title size="xl" className="                                       text-amber-300">
+        this is heading
+      </Title>
+    </div>
+  );
 }

415ee9fe4141ff868f62a6d876f759f234c49b34
Hossam Hamad <hsh02928@gmail.com>
2026-03-06 16:58:17 +0200

    Feat: Updata Navigation Bar the pages in navbar

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -3,6 +3,6 @@ export const Routes = [
   { id: 1, key: "Home", path: "/" },
   { id: 2, key: "About Us", path: "/about" },
   { id: 3, key: "Contact Us", path: "/contactus" },
-  { id: 4, key: "FAQs", path: "/FAQS" },
+  { id: 4, key: "FAQs", path: "/faqs" },
   { id: 5, key: "Pricing", path: "/pricing" },
 ];

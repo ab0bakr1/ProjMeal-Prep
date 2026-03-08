c105b70387831ba8f80e128da1d498cba5eb3195
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-08 22:40:50 +0300

    feature/branch : 3 Cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
+import Text from "@/components/atoms/Text";
 import PublicLayout from "@/components/layout/PublicLayout";
-
+import Boxes from "@/components/molecules/Boxes";
+import Heading from "@/components/molecules/Heading";
+import { dataBoxes } from "@/utils/data";
 
 export default function Page() {
   return (
     <div>
-      <PublicLayout>page</PublicLayout>
+      <PublicLayout>
+        <Heading />
+        <div className="ds-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+          {dataBoxes.map((box) => (
+            <Boxes key={box.id} title={box.title} text={box.text} />
+          ))}
+        </div>
+      </PublicLayout>
     </div>
   );
 }
